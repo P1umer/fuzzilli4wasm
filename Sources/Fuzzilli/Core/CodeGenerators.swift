b87e0f32a9e72cf777f4f6bf239fa32ad458c843
P1umer <1226063714@qq.com>
2022-04-08 02:08:04 +0800

    [Generator] Replace buffer source with fixed array

--- Sources/Fuzzilli/Core/CodeGenerators.swift
+++ Sources/Fuzzilli/Core/CodeGenerators.swift
@@ -993,9 +993,11 @@ public let CodeGenerators: [CodeGenerator] = [
     },
     
     CodeGenerator("BufferSourceGenerator") { b in
+        let buffer = [0,97,115,109,1,0,0,0,1,133,128,128,128,0,1,96,0,1,127,3,130,128,128,128,0,1,0,4,132,128,128,128,0,1,112,0,0,5,131,128,128,128,0,1,0,1,6,129,128,128,128,0,0,7,145,128,128,128,0,2,6,109,101,109,111,114,121,2,0,4,109,97,105,110,0,0,10,138,128,128,128,0,1,132,128,128,128,0,0,65,42,11]
+
         var initialValues = [Variable]()
-        for index in 0..<10 {
-            initialValues.append(b.loadInt(Int64(Int.random(in: 0...42))))
+        for index in 0..<buffer.count {
+            initialValues.append(b.loadInt(Int64(buffer[index])))
         }
         let AlterableArray = b.createArray(with: initialValues)
         let constructor = b.loadBuiltin("Uint8Array")
